c642e30cc710dc0e3e89e0a1c0aad36cf52d4fba
Ikuo Matsumura <matsumura_ikuo@cyberagent.co.jp>
2015-06-13 23:41:41 +0900

    Registering buffers

--- src/main/scala/buffercluster/Buffer.scala
+++ src/main/scala/buffercluster/Buffer.scala
@@ -6,12 +6,17 @@ import ClusterEvent._
 
 class Buffer extends Actor {
   val cluster = Cluster.get(context.system)
-
   override def preStart: Unit = cluster.subscribe(self, classOf[MemberUp])
   override def postStop: Unit = cluster.unsubscribe(self)
 
   def receive = {
+    case (key: String, value: String) => println(key)
     case state: CurrentClusterState => println(s"${state}============================================================")
-    case mUp: MemberUp => println(s"${mUp}============================================================")
+    case MemberUp(member) =>
+      println(s"MemberUp ${member}")
   }
+}
+
+object Buffer {
+  case class Put(key: String, value: String)
 }
--- src/main/scala/buffercluster/Frontend.scala
+++ src/main/scala/buffercluster/Frontend.scala
@@ -1,12 +1,25 @@
 package buffercluster
 
+import akka.cluster._
 import akka.actor._
+import ClusterEvent._
 
-class Frontend extends Actor {
-  private val backends = List[ActorRef]()
-  var jobCounter = 0
+class Frontend extends Actor with ActorLogging {
+  val cluster = Cluster.get(context.system)
+  override def preStart: Unit = cluster.subscribe(self, classOf[MemberUp])
+  override def postStop: Unit = cluster.unsubscribe(self)
+  private var buffers: List[ActorSelection] = Nil
 
   def receive = {
-    case (key: String, value: String) => println(key)
+    case msg : Buffer.Put => buffers.foreach(_ ! msg)
+    case state: CurrentClusterState => println(s"${state}============================================================")
+    case MemberUp(member) if member.hasRole("buffer") =>
+      log.info(s"Registering a buffer ${member}")
+      val actorRef = context.actorSelection(member.address + "/user/buffer")
+      this.buffers = actorRef :: buffers
+      dispatch(Buffer.Put("key2", "value2"))
   }
+
+  private def dispatch(msg: Buffer.Put): Unit =
+    this.buffers.foreach(_ ! msg)
 }
