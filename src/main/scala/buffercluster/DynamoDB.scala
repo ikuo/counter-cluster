597d6dc1d1b488bbb76eca6066f79429eb23f9bd
Ikuo Matsumura <matsumura_ikuo@cyberagent.co.jp>
2015-06-15 22:44:11 +0900

    Add createTables

--- src/main/scala/buffercluster/DynamoDB.scala
+++ src/main/scala/buffercluster/DynamoDB.scala
@@ -5,6 +5,8 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
 import com.amazonaws.services.dynamodbv2.{document => aws}
+import com.amazonaws.services.dynamodbv2.model._
+import scala.collection.JavaConversions._
 
 object DynamoDB {
   private val provider = new DefaultAWSCredentialsProviderChain()
@@ -16,4 +18,20 @@ object DynamoDB {
   client.setEndpoint("http://localhost:8000", "", "ap-northeast-1");
   val mapper: DynamoDBMapper = new DynamoDBMapper(client);
   val db: aws.DynamoDB = new aws.DynamoDB(client)
+
+  def createTables: Unit = {
+    val keySchema:List[KeySchemaElement] = List(
+      new KeySchemaElement().withAttributeName("key").withKeyType(KeyType.HASH)
+    )
+    val result = DynamoDB.client.createTable(
+      new CreateTableRequest("entries", keySchema)
+        .withAttributeDefinitions(
+          new AttributeDefinition().withAttributeName("key").withAttributeType(ScalarAttributeType.N)
+        )
+        .withProvisionedThroughput(
+          new ProvisionedThroughput().withReadCapacityUnits(4l).withWriteCapacityUnits(2l)
+        )
+    )
+    println(result)
+  }
 }
