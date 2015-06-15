64c204ed7f924b9b8858476e556cdaa007141544
Ikuo Matsumura <matsumura_ikuo@cyberagent.co.jp>
2015-06-16 01:01:22 +0900

    Use dynamodb-local

--- src/main/scala/buffercluster/DynamoDB.scala
+++ src/main/scala/buffercluster/DynamoDB.scala
@@ -16,8 +16,8 @@ object DynamoDB {
     override def getConnectionTimeout: Int = 2000
   }
   val client: AmazonDynamoDBClient = new AmazonDynamoDBClient(provider.getCredentials, configuration)
-  client.setRegion(Regions.AP_NORTHEAST_1)
-  //client.setEndpoint("http://localhost:8000", "", "ap-northeast-1");
+  //client.setRegion(Regions.AP_NORTHEAST_1)
+  client.setEndpoint("http://localhost:8000", "", "ap-northeast-1");
   val mapper: DynamoDBMapper = new DynamoDBMapper(client);
   val db: aws.DynamoDB = new aws.DynamoDB(client)
 
