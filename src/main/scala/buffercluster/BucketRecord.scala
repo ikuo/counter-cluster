b1bed9135826643ad65ae99ffeab0caea276ced3
Ikuo Matsumura <matsumura_ikuo@cyberagent.co.jp>
2015-06-16 00:56:16 +0900

    Add query

--- src/main/scala/buffercluster/BucketRecord.scala
+++ src/main/scala/buffercluster/BucketRecord.scala
@@ -1,26 +1,19 @@
 package buffercluster
-
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable
+import com.amazonaws.services.dynamodbv2.datamodeling._
 
 @DynamoDBTable(tableName = "entries")
-class BucketRecord(key: String, bucketId: Int, content: String) {
-  private var _key: String = key
-  private var _bucketId: Int = bucketId
-  private var _content: String = content
-  def this() { this("", -1, "") }
+case class BucketRecord(var key: String, var bucketId: Int, var content: String) {
+  def this() = this("", -1, "")
 
   @DynamoDBHashKey
-  def getKey = _key
-  def setKey(v: String): Unit = { this._key = v }
+  def getKey = key
+  def setKey(v: String): Unit = { this.key = v }
 
   @DynamoDBRangeKey(attributeName= "bucket-id")
-  def getBucketId = _bucketId
-  def setBucketId(v: Int): Unit = { this._bucketId = v; }
+  def getBucketId = bucketId
+  def setBucketId(v: Int): Unit = { this.bucketId = v; }
 
   @DynamoDBAttribute
-  def getContent = _content
-  def setContent(v: String): Unit = { this._content = v; }
+  def getContent = content
+  def setContent(v: String): Unit = { this.content = v; }
 }
--- src/main/scala/buffercluster/DynamoDB.scala
+++ src/main/scala/buffercluster/DynamoDB.scala
@@ -6,6 +6,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
 import com.amazonaws.services.dynamodbv2.{document => aws}
 import com.amazonaws.services.dynamodbv2.model._
+import com.amazonaws.regions.Regions
 import scala.collection.JavaConversions._
 
 object DynamoDB {
@@ -15,7 +16,8 @@ object DynamoDB {
     override def getConnectionTimeout: Int = 2000
   }
   val client: AmazonDynamoDBClient = new AmazonDynamoDBClient(provider.getCredentials, configuration)
-  client.setEndpoint("http://localhost:8000", "", "ap-northeast-1");
+  client.setRegion(Regions.AP_NORTHEAST_1)
+  //client.setEndpoint("http://localhost:8000", "", "ap-northeast-1");
   val mapper: DynamoDBMapper = new DynamoDBMapper(client);
   val db: aws.DynamoDB = new aws.DynamoDB(client)
 
