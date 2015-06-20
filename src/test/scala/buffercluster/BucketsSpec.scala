5bc5387eec47e8ef36697ebbe816f65e55e3efb4
Ikuo Matsumura <matsumura_ikuo@cyberagent.co.jp>
2015-06-20 17:38:41 +0900

    Add spec on multi-buckets

--- src/test/scala/buffercluster/BucketsSpec.scala
+++ src/test/scala/buffercluster/BucketsSpec.scala
@@ -7,30 +7,50 @@ import ExecutionContext.Implicits.global
 
 class BucketsSpec extends SpecificationLike {
   "Buckets" >> {
-    "#loadBuckets" >> {
-      "it saves content" in new BucketsCheck {
-        it.put("key001", "value001")
-        it.put("key001", "value002")
-        it.put("key003", "value003")
-        it.save.foreach(Await.result(_, 2.seconds))
-        Await.result(it.load, 2.seconds)
-        it.buckets.size must be_==(1)
-        it.buckets(0).pieces.size must be_==(2)
-        it.buckets(0).pieces must not contain(Buckets.Piece("key001", "value001"))
-        it.buckets(0).pieces must contain(Buckets.Piece("key001", "value002"))
-        it.buckets(0).pieces must contain(Buckets.Piece("key003", "value003"))
+    "#{seva, load}Buckets" >> {
+      "when small data" >> {
+        "it saves & load with a single bucket" in new BucketsCheck {
+          val it = new TargetBucket("single-bucket-test")
+          it.put("key001", "value001")
+          it.put("key001", "value002")
+          it.put("key003", "value003")
+          val saved = it.save
+          saved.size must be_==(1)
+          saved.foreach(Await.result(_, 5.seconds))
+          Await.result(it.load, 5.seconds)
+
+          it.buckets.size must be_==(1)
+          it.buckets(0).pieces.size must be_==(2)
+          it.buckets(0).pieces must not contain(Buckets.Piece("key001", "value001"))
+          it.buckets(0).pieces must contain(Buckets.Piece("key001", "value002"))
+          it.buckets(0).pieces must contain(Buckets.Piece("key003", "value003"))
+        }
+      }
+
+      "when large data" >> {
+        "it saves and loads with bultiple buckets" in new BucketsCheck {
+          val it = new TargetBucket("multi-bucket-test")
+          (1 to 100).foreach(i => it.put(s"key$i", s"value$i"))
+          val saved = it.save
+          saved.size must be_>(1)
+          saved.foreach(Await.result(_, 5.seconds))
+          Await.result(it.load, 5.seconds)
+
+          it.buckets.size must be_==(saved.size)
+          it.buckets(0).pieces.size must be_>(1)
+          it.buckets(1).pieces.size must be_>(0)
+        }
       }
     }
   }
 
   trait BucketsCheck extends Scope {
-    class TargetBucket extends Buckets {
-      override val persistenceId = "buckets01"
+    class TargetBucket(id: String) extends Buckets {
+      override val persistenceId = id
       override val ec = ExecutionContext.Implicits.global
       def put(key: String, value: String) = createOrUpdateBucketAndPiece(key, value)
       def save = saveBuckets
       def load = loadBuckets
     }
-    val it = new TargetBucket
   }
 }
