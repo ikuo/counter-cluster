1cde7db7694f6215fc8744cfe60cdf18f87b2b7b
Ikuo Matsumura <makiczar@gmail.com>
2015-06-27 15:34:16 +0900

    Switch dynamodb-local and aws remote

--- src/main/scala/countercluster/DynamoDB.scala
+++ src/main/scala/countercluster/DynamoDB.scala
@@ -8,17 +8,19 @@ import com.amazonaws.services.dynamodbv2.{document => aws}
 import com.amazonaws.services.dynamodbv2.model._
 import com.amazonaws.regions.Regions
 import scala.collection.JavaConversions._
+import com.typesafe.config.ConfigFactory
 
 object DynamoDB {
   private val provider = new DefaultAWSCredentialsProviderChain()
-  private val configuration = new ClientConfiguration() {
+  private val awsConfig = new ClientConfiguration() {
     override def getMaxErrorRetry: Int = 3
     override def getConnectionTimeout: Int = 2000
   }
-  val client: AmazonDynamoDBClient = new AmazonDynamoDBClient(provider.getCredentials, configuration)
-  //client.setRegion(Regions.AP_NORTHEAST_1)
-  client.setEndpoint("http://localhost:8000", "", "ap-northeast-1");
-  val mapper: DynamoDBMapper = new DynamoDBMapper(client);
+  val client: AmazonDynamoDBClient = new AmazonDynamoDBClient(provider.getCredentials, awsConfig)
+  val config = ConfigFactory.load.getConfig("counter-cluster.counter.dynamodb")
+  val endpoint = config.getString("endpoint")
+  client.setEndpoint(endpoint, "", "ap-northeast-1")
+  val mapper: DynamoDBMapper = new DynamoDBMapper(client)
   val db: aws.DynamoDB = new aws.DynamoDB(client)
 
   def createTables: Unit = {
