bc3c33e6c35201ea5e7d1de34f73c90e089a8d88
Ikuo Matsumura <makiczar@gmail.com>
2015-07-12 08:25:57 +0000

    Set user on docker build/run

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 user=${1:-`whoami`}
 cp ./target/scala-2.11/counter-cluster-assembly-0.1-SNAPSHOT.jar ./docker/
-sudo docker build -t $(whoami)/counter-cluster docker
+sudo docker build -t $user/counter-cluster docker
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-docker run -it --rm \
+user=${1:-`whoami`}
+sudo docker run -it --rm \
   --env="AKKA_HOSTNAME=seed0" \
   --env="AKKA_PORT=2551" \
   --env="CONFIG=/seed0.conf" \
@@ -7,4 +8,4 @@ docker run -it --rm \
   --add-host="seed0:127.0.0.1" \
   -p 2551 \
   --name=seed0 \
-  $(whoami)/counter-cluster
+  $user/counter-cluster
