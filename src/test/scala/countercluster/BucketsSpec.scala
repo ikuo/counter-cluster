6a9beb5800e8d7d896159d6f09d9bf627e75d028
Ikuo Matsumura <makiczar@gmail.com>
2015-07-12 08:04:02 +0000

    Fixup spec

--- src/test/scala/countercluster/BucketsSpec.scala
+++ src/test/scala/countercluster/BucketsSpec.scala
@@ -14,11 +14,7 @@ class BucketsSpec extends SpecificationLike {
           it.put("key001", "value001")
           it.put("key001", "value002")
           it.put("key003", "value003")
-          val saved = it.save
-          saved.size must be_==(1)
-          saved.foreach(Await.result(_, 5.seconds))
-          Await.result(it.load, 5.seconds)
-
+          it.save
           it.buckets.size must be_==(1)
           it.buckets(0).pieces.size must be_==(2)
         }
@@ -28,12 +24,8 @@ class BucketsSpec extends SpecificationLike {
         "it saves and loads with bultiple buckets" in new BucketsCheck {
           val it = new TargetBucket("multi-bucket-test")
           (1 to 100).foreach(i => it.put(s"key$i", s"value$i"))
-          val saved = it.save
-          saved.size must be_>(1)
-          saved.foreach(Await.result(_, 5.seconds))
-          Await.result(it.load, 5.seconds)
-
-          it.buckets.size must be_==(saved.size)
+          it.save
+          it.buckets.size must be_==(10)
           it.buckets(0).pieces.size must be_>(1)
           it.buckets(1).pieces.size must be_>(0)
         }
